d5127f363b9ac58f2640b4d45eee916facb11e90
Alexgrin216 <126878501+Alexgrin216@users.noreply.github.com>
2023-03-26 20:14:05 +0700

    Do Homework 3. It was hard like hell for me

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Lesson3;
+
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        Patient p1 = new Patient("Иван", 23, "Алеутская 4");
+        p1.disease.StomachDis = false;
+        p1.disease.EyeDis = true;
+        p1.disease.HearthDis = false;
+
+        Patient p2 = new Patient("Лиля", 18, "9 мая 34");
+        p2.disease.StomachDis = true;
+        p2.disease.EyeDis = false;
+        p2.disease.HearthDis = false;
+
+        Patient p3 = new Patient("Максим", 56, "Захарова 78");
+        p3.disease.StomachDis = false;
+        p3.disease.EyeDis = false;
+        p3.disease.HearthDis = true;
+
+        Ophtalm d1 = new Ophtalm("Олег", 42, "Окулист", 12);
+        d1.Cure(p1.disease);
+        Cardio d2 = new Cardio("Елена", 36, "Кардиолог", 9);
+        d2.Cure(p3.disease);
+        Gastroent d3 = new Gastroent("Глеб", 53, "Гастроэнтеролог", 23);
+        d3.Cure(p2.disease);
+    }
+
+    
+}
