ebb17672f9ce542b029e0b5916491c53867630ea
Alexgrin216 <126878501+Alexgrin216@users.noreply.github.com>
2023-04-01 11:35:24 +0700

    Add HW4

---
+++ Lesson4/Lesson4/Handler1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class Handler1
+    {
+        public void Message() 
+        {
+            Console.WriteLine($"Вы нашли ответ на все вопросы вселенной! Это 42!");
+        }
+    }
+}
